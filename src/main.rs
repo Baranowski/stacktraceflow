16ffafe998d055767d9ac0723043010605e1b231
Wojciech Baranowski <wbaranowski@protonmail.com>
2019-10-04 17:24:25 +0300

    Displaying basic tree_view

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use structopt::StructOpt;
 use std::io::BufRead;
 
+use cursive;
+use cursive_tree_view;
+
 /// Analyze and pretty-print StackTraceFlow data from a Rust program
 #[derive(StructOpt)]
 struct Cli {
@@ -19,6 +22,10 @@ fn main() {
     let reader = std::io::BufReader::new(file);
     let mut stack: Vec<String> = Vec::new();
     let mut counter: u32 = 1;
+    let mut tree = cursive_tree_view::TreeView::<String>::new();
+    let mut tree_stack : Vec<usize> = Vec::new();
+    tree_stack.push(0);
+
     for line in reader.lines() {
         let line = line.unwrap();
         if line.starts_with("+") {
@@ -30,6 +37,16 @@ fn main() {
                     indent=(stack.len()-1)*4
                 );
             }
+            let new_row_opt = tree.insert_item(
+                stack.last().unwrap().clone(),
+                cursive_tree_view::Placement::LastChild,
+                *tree_stack.last().unwrap(),
+            );
+            let new_row = match new_row_opt {
+                Some(x) => x,
+                None => !0,
+            };
+            tree_stack.push(new_row);
         } else if line.starts_with("-") {
             if stack.last() != Some(&line[1..].to_owned()) {
                 panic!(
@@ -40,9 +57,14 @@ fn main() {
                 );
             }
             stack.pop();
+            tree_stack.pop();
         } else {
             panic!("Line '{}' starts with neither '+' nor '-' in line {}", line, counter);
         }
         counter += 1;
     }
+
+    let mut siv = cursive::Cursive::default();
+    siv.add_layer(tree);
+    siv.run();
 }
