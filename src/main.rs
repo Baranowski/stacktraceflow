38fcc9027590956c37da156c59d3c4b10c6f6fe5
Wojciech Baranowski <wbaranowski@protonmail.com>
2019-10-05 01:06:15 +0300

    Failed attempt at polymorphism

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use structopt::StructOpt;
 use std::io::BufRead;
 
-use cursive;
-use cursive_tree_view;
+use atty;
+
+mod output;
 
 /// Analyze and pretty-print StackTraceFlow data from a Rust program
 #[derive(StructOpt)]
@@ -22,31 +23,19 @@ fn main() {
     let reader = std::io::BufReader::new(file);
     let mut stack: Vec<String> = Vec::new();
     let mut counter: u32 = 1;
-    let mut tree = cursive_tree_view::TreeView::<String>::new();
-    let mut tree_stack : Vec<usize> = Vec::new();
-    tree_stack.push(0);
+    let mut output: output::Output = if atty::is(atty::Stream::Stdout) { output::Tui::new() } else { output::Text::new() };
 
     for line in reader.lines() {
         let line = line.unwrap();
         if line.starts_with("+") {
             stack.push(line[1..].to_owned());
-            if stack.len() <= args.depth as usize {
-                println!(
-                    "{:indent$}{name}", "",
-                    name=stack.last().unwrap(),
-                    indent=(stack.len()-1)*4
-                );
-            }
-            let new_row_opt = tree.insert_item(
-                stack.last().unwrap().clone(),
-                cursive_tree_view::Placement::LastChild,
-                *tree_stack.last().unwrap(),
+            let transposed = stack.last().unwrap().split('@').rfold(
+                "".to_string(),
+                |accum, next| { accum + " " + next },
             );
-            let new_row = match new_row_opt {
-                Some(x) => x,
-                None => !0,
-            };
-            tree_stack.push(new_row);
+            if stack.len() <= args.depth as usize {
+                output.add_node(&transposed, stack.len()-1);
+            }
         } else if line.starts_with("-") {
             if stack.last() != Some(&line[1..].to_owned()) {
                 panic!(
@@ -57,14 +46,12 @@ fn main() {
                 );
             }
             stack.pop();
-            tree_stack.pop();
+            output.close_level();
         } else {
             panic!("Line '{}' starts with neither '+' nor '-' in line {}", line, counter);
         }
         counter += 1;
     }
 
-    let mut siv = cursive::Cursive::default();
-    siv.add_layer(tree);
-    siv.run();
+    output.finalize();
 }
